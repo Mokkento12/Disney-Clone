332bce1d3b72858fce6cf2a09323d9baa7b5f482
Roman Alekseev <roma.alekseev.90@bk.ru>
2023-09-26 11:21:18 +0300

    create: slider

--- js/main.js
+++ js/main.js
@@ -60,4 +60,19 @@ document.addEventListener("DOMContentLoaded", () => {
       video.pause();
     });
   });
+
+  let cardContainers = [...document.querySelectorAll(".card-container")];
+  let preBtns = [...document.querySelectorAll(".pre-btn")];
+  let nxtBtns = [...document.querySelectorAll(".nxt-btn")];
+
+  cardContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+    nxtBtns[i].addEventListener("click", () => {
+      item.scrollLeft += containerWidth - 200;
+    });
+    preBtnsBtns[i].addEventListener("click", () => {
+      item.scrollLeft -= containerWidth + 200;
+    });
+  });
 });
